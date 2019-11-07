23a22b7d2abf6aa988f767591f0b92b4a05b3b10
Utkarsh0203 <ag.utkarsh0203@gmail.com>
2019-11-07 06:29:12 +0530

    SpellChecker-v2

--- spellchecker.py
+++ spellchecker.py
@@ -10,21 +10,12 @@ aa = wordlist.words()
 
 def words(text): return re.findall(r'\S+', text.lower())
 
-WORDS = words(open('dictionary_lower.txt').read())
-
-# def P(word, N=sum(WORDS.values())): 
-#     "Probability of `word`."
-#     print(N, " is N")
-#     return WORDS[word] / N
-
-# def correction(word):
-#     "Most probable spelling correction for word."
-#     return max(candidates(word), key=P)
+WORDS = Counter(words(open('dictionary_lower.txt').read()))
 
 def candidates(word):
     "Generate possible spelling corrections for word."
-    a = list(known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
-    if ((len(a) is 1) and (a[0] is word)):
+    a = list(known([word]) or known(edits1(word)) or known(edits2(word)) or (set([word])))
+    if ((len(a) is 1) and (a[0] in WORDS)):
         return []
     else:
         return a
@@ -47,71 +38,10 @@ def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
-def suggestions(sentense):
+def suggestions(sentence):
     arr_suggestions = []
-    for word in sentense:
+    for word in sentence:
         processed_word = re.findall(r'\b(\S+)\b', word.lower())
         arr_suggestions.append(candidates(processed_word[0]))
+
     return arr_suggestions
-
-################ Test Code 
-
-# def unit_tests():
-#     assert correction('speling') == 'spelling'              # insert
-#     assert correction('korrectud') == 'corrected'           # replace 2
-#     assert correction('bycycle') == 'bicycle'               # replace
-#     assert correction('inconvient') == 'inconvenient'       # insert 2
-#     assert correction('arrainged') == 'arranged'            # delete
-#     assert correction('peotry') =='poetry'                  # transpose
-#     assert correction('peotryy') =='poetry'                 # transpose + delete
-#     assert correction('word') == 'word'                     # known
-#     assert correction('quintessential') == 'quintessential' # unknown
-#     assert words('This is a TEST.') == ['this', 'is', 'a', 'test']
-    # assert Counter(words('This is a test. 123; A TEST this is.')) == (
-    #        Counter({'123': 1, 'a': 2, 'is': 2, 'test': 2, 'this': 2}))
-#     assert len(WORDS) == 32192
-#     assert sum(WORDS.values()) == 1115504
-#     assert WORDS.most_common(10) == [
-#      ('the', 79808),
-#      ('of', 40024),
-#      ('and', 38311),
-#      ('to', 28765),
-#      ('in', 22020),
-#      ('a', 21124),
-#      ('that', 12512),
-#      ('he', 12401),
-#      ('was', 11410),
-#      ('it', 10681)]
-#     assert WORDS['the'] == 79808
-#     assert P('quintessential') == 0
-#     assert 0.07 < P('the') < 0.08
-#     return 'unit_tests pass'
-
-# def spelltest(tests, verbose=False):
-#     "Run correction(wrong) on all (right, wrong) pairs; report results."
-#     import time
-#     start = time.clock()
-#     good, unknown = 0, 0
-#     n = len(tests)
-#     for right, wrong in tests:
-#         w = correction(wrong)
-#         good += (w == right)
-#         if w != right:
-#             unknown += (right not in WORDS)
-#             if verbose:
-#                 print('correction({}) => {} ({}); expected {} ({})'
-#                       .format(wrong, w, WORDS[w], right, WORDS[right]))
-#     dt = time.clock() - start
-#     print('{:.0%} of {} correct ({:.0%} unknown) at {:.0f} words per second '
-#           .format(good / n, n, unknown / n, n / dt))
-    
-# def Testset(lines):
-#     "Parse 'right: wrong1 wrong2' lines into [('right', 'wrong1'), ('right', 'wrong2')] pairs."
-#     return [(right, wrong)
-#             for (right, wrongs) in (line.split(':') for line in lines)
-#             for wrong in wrongs.split()]
-
-# if __name__ == '__main__':
-#     print(unit_tests())
-#     spelltest(Testset(open('spell-testset1.txt')))
-#     spelltest(Testset(open('spell-testset2.txt')))
